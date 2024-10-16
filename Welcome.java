9d8949a405853507c18b1b448ccb71762ace9d9b
U1320165 <mohammad-imran.a.khan@mmc.com>
2024-10-16 12:20:00 +0530

    another commit

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,7 @@
 public class Welcome{
     public static void main(String[] args) {
         System.err.println("Welcome to JAVA App !!JENKINS");
+        System.err.println("New changes!");
+
     }
 }
